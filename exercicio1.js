d54b01e04c070482287e23b73ba8c9c9e931ebe9
andrepgfon <andrepgfon@gmail.com>
2018-04-19 11:47:18 -0300

    Final do Espaço - Morte de Calor

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,18 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+
+
+let botoesExpandir = document.querySelectorAll('.botao-expandir-retrair');
+console.log(botoesExpandir);
+for (let botao of botoesExpandir) {
+  botao.addEventListener('click', function(e) {
+    if(botao.parentNode.classList.length == 0){
+      botao.parentNode.classList.add('expandido');
+      botao.innerHTML = '-';
+    }else{
+      botao.parentNode.classList.remove('expandido');
+      botao.innerHTML = '+';
+    }
+
+  });
+}
--- exercicio2.js
+++ exercicio2.js
@@ -1,11 +1,26 @@
 // Faça o exercício da galeria de imagens aqui
 // Este arquivo ainda não está incluído no arquivo HTML
 
-const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
-  todasAsImagens = [
-    'philae-parts.jpg',
-    'philae-rosetta.jpg',
-    'philae-separation.jpg',
-    'philae-67-picture.jpg',
-    'philae-collecting.jpg'
-  ];
+const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/';
+const todasAsImagens = [
+  'philae-parts.jpg',
+  'philae-rosetta.jpg',
+  'philae-separation.jpg',
+  'philae-67-picture.jpg',
+  'philae-collecting.jpg'
+];
+let imageIndex = 0;
+let botaoProx = document.querySelector('#proximo');
+let botaoAnt = document.querySelector('#anterior');
+let slide = document.querySelector('#slide')
+botaoProx.addEventListener('click', function(e) {
+  imageIndex = (imageIndex + 1)%5;
+  slide.src = servidorDasImagens+todasAsImagens[imageIndex];
+});
+botaoAnt.addEventListener('click', function(e) {
+  imageIndex = imageIndex - 1;
+  if(imageIndex < 0){
+    imageIndex = 4;
+  }
+  slide.src = servidorDasImagens+todasAsImagens[imageIndex];
+});
